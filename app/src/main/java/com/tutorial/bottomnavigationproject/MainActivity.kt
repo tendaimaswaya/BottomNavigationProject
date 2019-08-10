12392b8bd0dc4b36312f6d8e5a6f2f0619796b87
Tendai Maswaya <maswayatm@gmail.com>
2019-08-10 13:48:41 +0200

    Update MainActivity.kt
    removed unneccessary imports

--- app/src/main/java/com/tutorial/bottomnavigationproject/MainActivity.kt
+++ app/src/main/java/com/tutorial/bottomnavigationproject/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.tutorial.bottomnavigationproject
 
 import android.annotation.SuppressLint
-import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -9,21 +8,11 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
-import android.support.v7.app.AlertDialog
-import android.view.LayoutInflater
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ProgressBar
 import com.tutorial.bottomnavigationproject.fragments.FragmentOne
 import com.tutorial.bottomnavigationproject.fragments.FragmentThree
 import com.tutorial.bottomnavigationproject.fragments.FragmentTwo
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONException
-import org.json.JSONObject
-import java.lang.Exception
 import java.util.ArrayList
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,7 +21,7 @@ class MainActivity : AppCompatActivity() {
             setContentView(R.layout.activity_main)
             setupViewPager(pager)
         }
-
+    //the ViewPager will be responsible for navigating through your fragments even when you need to slide through them
         private fun setupViewPager(viewPager: ViewPager){
             val adapter = ViewPagerAdapter(supportFragmentManager)
             setTabs(adapter)
@@ -40,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             viewPager.adapter = adapter
             initTabLayout()
         }
-
+        //assign icons to the TabLayout
         @SuppressLint("NewApi")
         fun setTabIcons(){
             tabLayout.getTabAt(0)!!.setIcon(ContextCompat.getDrawable(applicationContext,R.mipmap.ic_launcher))
@@ -57,6 +46,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    //assign Fragments associated with a specific Tab Item
         private fun setTabs(adapter:ViewPagerAdapter){
             adapter.addFragment(FragmentOne(), "")
             adapter.addFragment(FragmentTwo(), "")
